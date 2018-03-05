98b7fab4b70870d8a7977c2de6f056bb1416c746
tolgahanuzun <mail@tolgahanuzun.com>
2018-03-06 00:07:31 +0300

    install result API

--- userfeed/userfeed.py
+++ userfeed/userfeed.py
@@ -1,4 +1,5 @@
 from datetime import datetime
+import json
 
 from sqlalchemy import create_engine, Column, Integer, String, Text, DateTime
 from sqlalchemy.ext.declarative.api import declarative_base
@@ -37,59 +38,49 @@ def user_details(name: hug.types.text,
                 post: hug.types.text,
                 transfer: hug.types.text,
                 hug_timer=3):
+    
+    # Control
 
     status = {
-        'vote' = vote,
-        'custom_json'= follow,
-        'comment' = post,
-        'transfer' = transfer,
-        'curation_reward' = True,
-        'claim_reward_balance' = True,
-        'author_reward' = True,
-
+        'vote' : vote,
+        'custom_json' : follow,
+        'comment' : post,
+        'transfer' : transfer,
+        'curation_reward' : True,
+        'claim_reward_balance' : True,
+        'author_reward' : True,
+        'account_update' : False,
+        'comment_benefactor_reward': False,
+        'transfer_to_vesting': False,
+        'account_witness_vote': False,
+        'limit_order_create': False,
+        'fill_order': False,
+        'limit_order_cancel': False,
+        'comment_options': False
     }
 
+    #cleaning
     feed_lists = hook.feed_list(name)
-    new_list = []
+    new_lists = []
     for feed in feed_lists:
-        new_list.append(feed[1]['op'])
-    return new_list
+        if status[feed[1]['op'][0]] == 'True':
+            new_lists.append(feed[1]['op'])
+    
+
+    posting = []
+    #posting
+    for nw in new_lists:
+        if nw[0] == 'vote':
+            post_link = "/@{}/{} ".format(nw[1]['author'], nw[1]['permlink'])
+            text_link =  "<a href='https://www.steemit.com{}'>{}</a>".format(post_link, nw[1]['permlink'])
+            text = "{} voteup it {}(%{})".format(nw[1]['voter'], text_link, float(nw[1]['weight']/100) )
+            posting.append(text)
+        if nw[0] == 'custom_json':
+            data = json.loads(nw[1]['json'])
+            text = '{1} following {0}'.format(data[1]['following'], data[1]['follower'])
+            posting.append(text)
+    return {'result': posting,
+            'took': float(hug_timer)}
 
 if __name__ == '__main__':
     user_details.interface.cli()
-
-
-
-#  """User Details"""
-
-#     user = session_factory.query(Model).filter_by(name=name).all()
-#     if user:
-#         last_update = datetime.now() - user[0].date
-        
-#         if last_update.seconds / 60 > 10:
-#             text = ''
-#             result = hook.hook(name)
-#             for hooks in result:
-#                 text = text + ':::::' + hooks
-#             user[0].body = text
-#             user[0].date = datetime.now()
-#             session_factory.add(user[0])
-#             session_factory.commit()
-#         else:
-#             result = user[0].body.split(':::::')[1:]
-
-#     else:
-#         text = ''
-#         result = hook.hook(name)
-#         for hooks in result:
-#             text = text + ':::::' + hooks
-
-#         user = Model()
-#         user.name = name
-#         user.body = text
-#         user.date = datetime.now()
-#         session_factory.add(user)
-#         session_factory.commit()
-
-#     return {'result': result,
-#             'took': float(hug_timer)}
